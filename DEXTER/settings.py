76903840e0a6900e76235703a99640ee97b8cf6f
TheHackerClown <dhruvprataps14@gmail.com>
2024-03-27 20:47:11 +0530

    DOne

--- DEXTER/settings.py
+++ DEXTER/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG','False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = list(os.environ.get('ALLOWED_HOSTS').split(' '))
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origins-allow-popups'
 APPEND_SLASH=False
 
